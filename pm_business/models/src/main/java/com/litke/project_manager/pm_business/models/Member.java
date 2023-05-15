0e504da71821a341d787c504be57ca7b2cbccbef
LitkeNE <nlitke1509@gmail.com>
2023-05-15 12:36:04 +0600

    added decomposition

--- pm_business/models/src/main/java/com/litke/project_manager/pm_business/models/Member.java
+++ pm_business/models/src/main/java/com/litke/project_manager/pm_business/models/Member.java
@@ -1,32 +1,11 @@
 package com.litke.project_manager.pm_business.models;
 
+import lombok.Data;
+
+@Data
 public class Member {
 
     private Long id;
     private String lastName;
     private String firstName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 }
